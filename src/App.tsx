df324b19a4bae74f7af33eb3a0c573f213efad8a
ivanagbarreto <ivanagbarreto@gmail.com>
2025-05-12 19:33:49 -0300

    se agrega el proyecto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import Navbar from './components/Navbar/Navbar.tsx';
+import Home from './Home.tsx';
+
+function App() {
+  return (
+    <>
+    
+    <Navbar/>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
+
