5c2ee020f28198a98c6c7fd0f4c13bb50dc119ed
ivanagbarreto <ivanagbarreto@gmail.com>
2025-05-22 19:02:40 -0300

    cambios en home- darkmode root

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import NavbarComp from './components/NavbarComp/NavbarComp.tsx';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import BusinessPage from './pages/Business/BusinessPage.tsx';
 import PreciosPage from './pages/Precios/PreciosPage.tsx';
-import Home from './Home.tsx';
+import Home from './pages/Home/Home.tsx';
 import './index.css'; 
 
 function App() {
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './components/Home.module.css';
+import styles from './Home.module.css';
 
 const Home: React.FC = () => {
   return (
