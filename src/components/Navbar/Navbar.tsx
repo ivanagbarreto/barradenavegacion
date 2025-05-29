f7125fdda7dbde918ed053a2e562abeb72ce8c2e
ivanagbarreto <ivanagbarreto@gmail.com>
2025-05-28 21:59:00 -0300

    cambio de logo - css

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -68,13 +68,47 @@ const Navbar: React.FC = () => {
         }`}
       >
         <NavLink to="/" className={styles.logoLink}>
-          <img
+          <svg
+            width="403"
+            height="403"
+            viewBox="0 0 403 403"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
             className={styles.logoImg}
-            width={32}
-            height={32}
-            src="https://mega.io/es/wp-content/themes/megapages/megalib/images/megaicon.svg"
-            alt="MEGA Logo"
-          />
+          >
+            <g clip-path="url(#clip0_31_2)">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M201.5 403C90.272 403 0 312.728 0 201.5C0 90.272 90.272 0 201.5 0C312.728 0 403 90.272 403 201.5C403 312.728 312.728 403 201.5 403Z"
+                fill="#5021FF"
+              />
+              <path
+                d="M161.241 262V141.065H246.273V167.52H194.073V188.305H241.076V214.76H194.073V262H161.241Z"
+                fill="white"
+              />
+              <path
+                d="M76.0867 197.468C76.0796 220.286 82.0383 242.563 93.3666 262.198L75 329.288L143.627 311.286C162.536 321.6 183.825 327.038 205.489 327.046H205.544C276.89 327.046 334.97 268.958 335 197.571C335.012 162.975 321.557 130.443 297.117 105.97C272.674 81.5 240.174 68.0142 205.544 68C134.187 68 76.1152 126.08 76.0867 197.468ZM116.956 258.816L114.394 254.747C103.622 237.611 97.9369 217.809 97.945 197.476C97.9673 138.141 146.234 89.8665 205.585 89.8665C234.327 89.8787 261.339 101.088 281.655 121.427C301.971 141.767 313.15 168.806 313.143 197.563C313.116 256.899 264.849 305.179 205.544 305.179H205.502C186.192 305.169 167.253 299.98 150.737 290.176L146.807 287.844L106.082 298.526L116.956 258.816Z"
+                fill="url(#paint0_linear_31_2)"
+              />
+            </g>
+            <defs>
+              <linearGradient
+                id="paint0_linear_31_2"
+                x1="13075"
+                y1="26196.8"
+                x2="13075"
+                y2="68"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#F9F9F9" />
+                <stop offset="1" stop-color="white" />
+              </linearGradient>
+              <clipPath id="clip0_31_2">
+                <rect width="403" height="403" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
         </NavLink>
         {/*********** BOTON HAMBURGUESA ****************/}
         <div className={styles.menuIcon} onClick={toggleMenu}>
@@ -132,9 +166,7 @@ const Navbar: React.FC = () => {
 
                         <div className={styles.textColumn}>
                           <h4>Password Manager</h4>
-                          <p>
-                            Protect passwords and access them on any device
-                          </p>
+                          <p>Protect passwords and access them on any device</p>
                         </div>
                       </div>
                       <div className={styles.menuItem}>
@@ -171,7 +203,7 @@ const Navbar: React.FC = () => {
                         </div>
                         <div className={styles.textColumn}>
                           <h4> Synchronize</h4>
-                          <p>Sync your files with MEGA</p>
+                          <p>Sync your files </p>
                         </div>
                       </div>
                       <div className={styles.menuItem}>
@@ -233,7 +265,8 @@ const Navbar: React.FC = () => {
                         <div className={styles.textColumn}>
                           <h4>Freelancers</h4>
                           <p>
-                            Creatives, contractors, consultants, and entrepreneurs
+                            Creatives, contractors, consultants, and
+                            entrepreneurs
                           </p>
                         </div>
                       </div>
@@ -245,7 +278,8 @@ const Navbar: React.FC = () => {
                         <div className={styles.textColumn}>
                           <h4>Small Business</h4>
                           <p>
-                            Startups, merchants, family businesses, and retailers
+                            Startups, merchants, family businesses, and
+                            retailers
                           </p>
                         </div>
                       </div>
@@ -300,7 +334,7 @@ const Navbar: React.FC = () => {
                         </div>
                         <div className={styles.textColumn}>
                           <h4>Help Center</h4>
-                          <p>Guidance and support for MEGA products</p>
+                          <p>Guidance and support </p>
                         </div>
                       </div>
                       <div className={styles.menuItem}>
@@ -318,7 +352,7 @@ const Navbar: React.FC = () => {
                         </div>
                         <div className={styles.textColumn}>
                           <h4>Blog</h4>
-                          <p>The latest news, tips, and ideas from MEGA</p>
+                          <p>The latest news, tips, and ideas </p>
                         </div>
                       </div>
                       <div className={styles.menuItem}>
@@ -338,7 +372,8 @@ const Navbar: React.FC = () => {
                         <div className={styles.textColumn}>
                           <h4>Reliability</h4>
                           <p>
-                            Find out how we keep your data accesible anytime, anywhere
+                            Find out how we keep your data accesible anytime,
+                            anywhere
                           </p>
                         </div>
                       </div>
@@ -350,7 +385,8 @@ const Navbar: React.FC = () => {
                         <div className={styles.textColumn}>
                           <h4>Developers</h4>
                           <p>
-                            Check out our source code and software development kit
+                            Check out our source code and software development
+                            kit
                           </p>
                         </div>
                       </div>
